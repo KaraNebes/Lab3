7f1ad39573eeac7ff4bb2fc112a187ea251cd09c
Karasik <ann.222@mail.ru>
2020-10-01 19:44:00 +0500

    Оно работает, но нет

--- Program.cs
+++ Program.cs
@@ -7,18 +7,23 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            double alfa=0, x1=0, x2=0, maxlength=0;
+            double alfa = 0, x1 = 0, x2 = 0;
+            int maxlength = 0;
             List<double> allx = new List<double>();
             List<double> ally = new List<double>();
             Read(out alfa, out x1, out x2);
             allx = CreatingAnXArray(alfa, x1, x2);
             ally = CreatingAnYArray(allx);
             int maxlengthx = FindingMaxLength(allx);
+            Console.WriteLine(maxlengthx);
             int maxlengthy = FindingMaxLength(ally);
+            Console.WriteLine(maxlengthy);
             if (maxlengthx > maxlengthy)
                 maxlength = maxlengthx;
             else
                 maxlength = maxlengthy;
+            Console.WriteLine(maxlength);
+            DrawingTable(allx, ally, maxlength);
             Console.ReadKey();
             //Math.Round(ally[i],3);
         }
@@ -31,19 +36,19 @@ namespace Lab3
             Console.WriteLine("Введите альфу");
             alfa = Convert.ToDouble(Console.ReadLine());
         }
-        static List<double> CreatingAnXArray (double alfa, double x1, double x2)
+        static List<double> CreatingAnXArray(double alfa, double x1, double x2)
         {
             List<double> allx = new List<double>();
             allx.Add(x1);
             int n = Convert.ToInt32((x2 - x1) / alfa);
-            for (int i=1; i <= n; i++)
+            for (int i = 1; i <= n; i++)
             {
                 allx.Add(x1 + (alfa * i));
             }
-            
-                return allx;
+
+            return allx;
         }
-        static List<double> CreatingAnYArray (List<double> allx)
+        static List<double> CreatingAnYArray(List<double> allx)
         {
             List<double> ally = new List<double>();
             for (int i = 0; i < allx.Count; i++)
@@ -52,24 +57,29 @@ namespace Lab3
             }
             return ally;
         }
-        static int FindingMaxLength (List<double> all)
+        static int FindingMaxLength(List<double> all)
         {
-            int digitlength = 0, fractionaldigitlength = 0, length = 0, maxlength = 0;
-            for(int i =0; i<all.Count; i++)
+            int length = 0, maxlength = 0;
+            for (int i = 0; i < all.Count; i++)
             {
-                length = 0;
-                if (all[i] < 0)
-                    length++;
-                digitlength = GetWholeDigitCount(all[i]);
-                fractionaldigitlength = GetFractionalDigitCount(all[i]);
-                length += digitlength + fractionaldigitlength;
+                length = FindingLength(all[i]);
                 if (length >= maxlength)
                     maxlength = length;
-                
             }
+            maxlength++;
             return maxlength;
         }
-        static int GetWholeDigitCount (double x)
+        static int FindingLength(double x)
+        {
+            int length = 0;
+            if (x < 0)
+                length++;
+            int digitlength = GetWholeDigitCount(x);
+            int fractionaldigitlength = GetFractionalDigitCount(x);
+            length += digitlength + fractionaldigitlength;
+            return length;
+        }
+        static int GetWholeDigitCount(double x)
         {
             int count = 1;
             x = Math.Abs(x);
@@ -79,7 +89,7 @@ namespace Lab3
         }
         static int GetFractionalDigitCount(double x)
         {
-            x = Math.Round(Math.Abs(x),3);
+            x = Math.Abs(x);
             int count = 0;
             while (x % 1 != 0)
             {
@@ -87,6 +97,55 @@ namespace Lab3
                 x *= 10;
             }
             return count;
+        }
+        static void PrintNTimes(char a, int n)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(a);
+            }
+        }
+
+        static void DrawingCell(double x, int maxlength)
+        {
+            int n = maxlength - FindingLength(x);
+            PrintNTimes(' ', n / 2);
+            if (FindingLength(x) % 2 != 0)
+                PrintNTimes(' ', 1);
+            Console.Write(x);
+            PrintNTimes(' ', n / 2);
+            Console.Write("|");
+        }
+        static void DrawingTable(List<double> allx, List<double> ally, int maxlength)
+        {
+            Console.Write("|");
+            for (int i = 0; i < 2; i++)
+            {
+                PrintNTimes('-', maxlength+1);
+                Console.Write("|");
+            }
+            Console.WriteLine();
+            Console.Write("|");
+            PrintNTimes(' ', maxlength / 2);
+            Console.Write('x');
+            PrintNTimes(' ', maxlength / 2);
+            Console.Write(" | ");
+            PrintNTimes(' ', maxlength / 2);
+            Console.Write('y');
+            PrintNTimes(' ', maxlength / 2);
+            Console.WriteLine("|");
+            for (int i = 0; i < 2 * maxlength+5; i++)
+            {
+                Console.Write('-');
+            }
+            Console.WriteLine();
+            for (int i = 0; i < allx.Count; i++)
+            {
+                Console.Write("|");
+                DrawingCell(allx[i], maxlength);
+                DrawingCell(ally[i], maxlength);
+                Console.WriteLine( );
+            }
         }
     }
 }
